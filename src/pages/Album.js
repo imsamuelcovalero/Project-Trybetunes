7c78535d895ba8acc2db9881610f864f5b7cdd3e
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2022-04-18 15:34:19 -0300

    resolve o requisito 1

---
+++ src/pages/Album.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Album extends Component {
+  render() {
+    return (
+      <div data-testid="page-album">
+        <h1>HOME PAGE</h1>
+      </div>
+    );
+  }
+}
+
+export default Album;
---
+++ src/pages/ProfileEdit.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProfileEdit extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ProfileEdit</h1>
+      </div>
+    );
+  }
+}
+
+export default ProfileEdit;
