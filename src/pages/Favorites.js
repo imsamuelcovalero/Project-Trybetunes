3b7d5f6830a182e17e90ce37d34095d1f8cf82e2
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2022-04-20 18:08:31 -0300

    resolve erros no requisito 3 e 4

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 class Favorites extends Component {
   render() {
     return (
       <div data-testid="page-favorites">
+        <Header />
         <h1>FAVORITES</h1>
       </div>
     );
